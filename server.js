15046a5a0a50becb52afd3c67624232a42740673
merimadanyan <81684592+merimadanyan@users.noreply.github.com>
2021-04-12 13:43:20 +0400

    Update server.js

--- server.js
+++ server.js
@@ -79,6 +79,21 @@ app.post('/data/delete', function (req, res) {
   .catch(console.error)
 })
 
+app.post('/data/update', function (req, res) {
+  client.connect()
+  .then(client => {
+    let id = req.body.id;
+    const query = { "_id": ObjectId(id)};
+    client.db('cse120-2021-db').collection('books').updateOne(query, {$set: req.body})
+      .then(result => {
+        console.log(result)
+        res.send({"status":"Updated"});
+      })
+      .catch(error => console.error(error))
+  })
+  .catch(console.error)
+})
+
 app.listen(port, function () {
     console.log('Example app listening on port 3001!')
 })
