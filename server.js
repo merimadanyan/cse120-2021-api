5f5a0b36c8318dc21b8899bb15142e3b05888fad
merimadanyan <81684592+merimadanyan@users.noreply.github.com>
2021-05-03 20:55:24 +0400

    changing the port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser  = require('body-parser');
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3002;
 const app = express();
 const cors = require('cors');
 const { MongoClient } = require("mongodb");
